aaa71cc3f7775839f5cdc2a45227c4bcf604b068
Prabhushapure <prabhu8374691023@gmail.com>
2026-05-22 11:59:19 +0530

    base name change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/quality_quiz/",
+  base: "/quality-quiz/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
